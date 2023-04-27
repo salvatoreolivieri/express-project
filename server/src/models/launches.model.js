974f29668145296ca9ba90808375caa7db835b52
Salvatore Olivieri <99086797+salvatoreolivieri@users.noreply.github.com>
2023-04-28 00:41:08 +0200

    implementing launches from spaceX API in mongoDB with pagination

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,9 @@
+const axios = require("axios")
 const launchesDatabase = require("./launches.mongo")
 const planets = require("./planets.mongo")
 
 const DEFAULT_FLIGHT_NUMBER = 100
-const launches = new Map()
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query"
 
 const launch = {
   flightNumber: 100,
@@ -15,27 +16,13 @@ const launch = {
   success: true,
 }
 
-const getLatestFlightNumber = async () => {
-  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber")
-
-  if (!latestLaunch) {
-    return DEFAULT_FLIGHT_NUMBER
-  }
-
-  return latestLaunch.flightNumber
+// Find
+const findLaunch = async (filter) => {
+  return await launchesDatabase.findOne(filter)
 }
 
-//  Save
+// Save
 const saveLaunch = async (launch) => {
-  // Check if the launch target planet exist in our Database
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  })
-
-  if (!planet) {
-    throw new Error("No matching planet found!") // This is a best practies: https://github.com/goldbergyoni/nodebestpractices
-  }
-
   await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
@@ -49,19 +36,109 @@ const saveLaunch = async (launch) => {
 
 saveLaunch(launch)
 
-// GET
-const getAllLaunches = async () => {
-  return await launchesDatabase.find(
-    {},
-    {
-      _id: 0,
-      __v: 0,
+// Populate
+const populateLaunches = async () => {
+  console.log("Downloading launch data...")
+
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  })
+
+  if (response.status !== 200) {
+    console.log("Problem downloading launch data")
+    throw new Error("Launch data download failed")
+  }
+
+  const launchDocs = response.data.docs
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"]
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"]
+    })
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
     }
-  )
+
+    console.log(`${launch.flightNumber}: ${launch.mission}`)
+
+    await saveLaunch(launch)
+  }
+}
+
+// Load
+const loadLaunchData = async () => {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  })
+
+  if (firstLaunch) {
+    console.log("launch data already loaded!")
+  }
+
+  await populateLaunches()
+}
+
+// GET
+const getLatestFlightNumber = async () => {
+  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber")
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER
+  }
+
+  return latestLaunch.flightNumber
+}
+
+const getAllLaunches = async (skip, limit) => {
+  return await launchesDatabase
+    .find(
+      {},
+      {
+        _id: 0,
+        __v: 0,
+      }
+    )
+    .skip(skip)
+    .limit(limit)
 }
 
 // POST
 const scheduleNewLaunch = async (launch) => {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  })
+
+  if (!planet) {
+    throw new Error("No matching planet found!") // This is a best practies: https://github.com/goldbergyoni/nodebestpractices
+  }
+
   const newFlightNumber = (await getLatestFlightNumber()) + 1
 
   const newLaunch = Object.assign(launch, {
@@ -76,7 +153,7 @@ const scheduleNewLaunch = async (launch) => {
 
 // DELETE
 const existLaunchWithId = async (launchId) => {
-  return await launchesDatabase.findOne({
+  return await findLaunch({
     flightNumber: launchId,
   })
 }
@@ -95,7 +172,9 @@ const abortLaunchById = async (launchId) => {
   return aborted.modifiedCount === 1 && aborted.matchedCount === 1
 }
 
+// EXPORT
 module.exports = {
+  loadLaunchData,
   getAllLaunches,
   scheduleNewLaunch,
   abortLaunchById,
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,9 +5,14 @@ const {
   existLaunchWithId,
 } = require("../../models/launches.model")
 
+const { getPagination } = require("../../services/query")
+
 // GET
 const httpGetAllLaunches = async (request, response) => {
-  return response.status(200).json(await getAllLaunches())
+  const { skip, limit } = getPagination(request.query)
+  const launches = await getAllLaunches(skip, limit)
+
+  return response.status(200).json(launches)
 }
 
 // POST
